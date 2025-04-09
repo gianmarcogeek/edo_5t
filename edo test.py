dc137791c34a62f9cee660b2efcc25e31b9363bf
Zel737 <alzelx04@gmail.com>
2025-04-09 15:44:58 +0200

    new version

--- edo test.py
+++ edo test.py
@@ -1,5 +1,28 @@
 from pyedo import edo
+import time
+
+myedo = edo('192.168.12.1') # create an instance of an eDO object with IP address
+
+x = float
+y = float
+
+### init ###
+myedo.init7Axes() 
+time.sleep(5)
+myedo.disengageStd() # release brakes and movement
+time.sleep(5)
+myedo.calibAxes() # make the zero on the axis (axis should be already aligned)
+
+
+### Subscription to the topics ###
+myedo.listenMovementAck() # Movement confirmation topic
+myedo.listenValues()# Space coordonates (x,y,z,A,E,R) topic
+
+myedo.moveToWaitingPos() # starting point for cartesian moves
+
+### move function ###
 if x <= 100:
-    myedo.moveCartesianX(200, 350, 150, 0, 180, 23.57, 60) 
+    myedo.moveCircularX(x, y, 150, 0, 180, 23.57, 60, 250, 400,150, 0, 180, 23.57, 60) 
 else: 
     myedo.moveCartesianX(x, y, 150, 0, 180, 23.57, 60)
+
